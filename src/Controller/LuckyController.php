42890dd57f272eac2a226e7144f9fa0191b55453
Romain <romain.cardasi@etu.umontpellier.fr>
2019-11-03 16:48:59 +0100

    Reparation

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,7 +1,6 @@
 <?php
 // src/Controller/LuckyController.php
 namespace App\Controller;
-require_once 'C:\Users\rcada\OneDrive\Bureau\VoltaireQ2Project\VoltaireQ2Project\src\model\ModelEtudiantComplexe.php';
 
 
 //Les importations de Sympfony
@@ -46,53 +45,39 @@ public function number()
 	$dateCV = "";
 
         // $fp is file pointer to file sample.csv
-	if (($fp = fopen("C:\\Users\\rcada\\OneDrive\\Bureau\\VoltaireQ2Project\\VoltaireQ2Project\\src\\Controller\\data.csv", "r")) !== FALSE) {
+	if (($fp = fopen((__DIR__)."\\data.csv", "r")) !== FALSE) {
 		while (($row = fgetcsv($fp, 1000, ";")) !== FALSE) {
             //Only 0 .  $num = count($row);
             //useless because CSV have one column and c is always only 0.  for($c=0 ; $c< $num; $c++){
 			$str = explode(";", $row[0]);
 			//explode va faire de str un array qui est row découpée: exemple 1;2;3;4;5 l'array "1","2", etc...
 			if($boolean == 0){
-				foreach ($str as $s) {
+				foreach ($str as $s) {// La premiere ligne contient les noms de colonnes
 					array_push($nomcolonnes,$s);
 					$boolean++;
+					
 				}
+				$nbColonnes = count($str);
 			}
-			$nbColonnes = count($str);
-			
-			
-			array_push($info, $str);
+			else{
+				foreach ($str as $s ) {
+					echo($s);
+					echo(" , ");
+				}
+				array_push($info, $str);
+			}
 			
 			$rowNo++;
+			echo("<br>");
 		}
 		fclose($fp);
 	}
-	$file = fopen("C:\\Users\\rcada\\OneDrive\\Bureau\\VoltaireQ2Project\\VoltaireQ2Project\\src\\Controller\\result.csv", "w");
+	$file = fopen((__DIR__)."\\result.csv", "w");
 	foreach ($info as $ligne) {
 		fputcsv($file, $ligne);
 	}
 	fclose($file);
-	foreach ($info as $ligne) {
-
-			$sphère = $ligne[0];
-			$groupe = $ligne[1];
-			$nom = $ligne[2];
-			$prenom = $ligne[3];
-			$identifiant = $ligne[4];
-			$inscription = $ligne[5];
-			$module = $ligne[6];
-			$dernutilisation = $ligne[7];
-			$tempstotal = $ligne[8];
-			$usagefixe = $ligne[9];
-			$usagemobile = $ligne[10];
-			$scoreevalinit = $ligne[11];
-			$tmpsevaluationinit = $ligne[12];
-			$niveauinit = $ligne[13];
-			$tmpentrainement = $ligne[14];
-			$niveauatteint = $ligne[15];
-			$dateCV = $ligne[16];
-			$model1 = new ModelEtudiantComplexe($sphère,$groupe,$nom,$prenom,$identifiant,$inscription,$module,$dernutilisation,$tempstotal,$usagefixe,$usagemobile,$scoreevalini,$tmpsevaluationinit,$niveauinit,$tmpentrainement,$niveauatteint,$dateCV);
-	}
+	
 
 }
 }?>
